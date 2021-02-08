fb848dbc714a837d52fd00dc414728c8508635a5
josepintado24 <josepintado24@gmail.com>
2021-02-08 16:54:07 -0500

    mensajes

---
+++ content/assets/js/sweetalert2.js
@@ -0,0 +1,35 @@
+$("#Pre_insc_01").click(function () {
+    Swal.fire({
+        text: "Se que estas entusiasmado, las inscripciones a la prueba de selección es el 15 de febrero",
+        icon: "info",
+        timer: 5000,
+        timerProgressBar: true
+    });
+});
+
+$("#Pre_insc_02").click(function () {
+    Swal.fire({
+        text: "Se que estas entusiasmado, las inscripciones a la prueba de selección es el 15 de febrero",
+        icon: "info",
+        timer: 5000,
+        timerProgressBar: true
+    });
+});
+
+$("#Pre_insc_03").click(function () {
+    Swal.fire({
+        text: "Se que estas entusiasmado, las inscripciones a la prueba de selección es el 15 de febrero",
+        icon: "info",
+        timer: 5000,
+        timerProgressBar: true
+    });
+});
+
+$("#Bas_insc_01").click(function () {
+    Swal.fire({
+        text: "Se que estas entusiasmado, las inscripciones a la prueba de selección es el 15 de febrero",
+        icon: "info",
+        timer: 5000,
+        timerProgressBar: true
+    });
+});
